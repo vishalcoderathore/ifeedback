078fa1e5ae6ca2449ae7bea4854d4fe1abb30105
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-04 21:36:09 -0600

    added proxy configuration for Google Strategy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.deserializeUser((id, done) => { // search db to for User model
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     User
         .findOne({googleId: profile.id})

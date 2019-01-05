79f30882f5d782ac16f943d71d82f70de52b9bef
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-04 21:16:55 -0600

    setup production environment

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// production environment credentials
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI : process.env.MONGO_URI,
+    cookieKey : process.env.COOKIE_KEY
+}

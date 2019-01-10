0a65d2a961da1e826ca856472d8741b58bb39e7b
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-10 14:23:03 -0600

    basic survey layout with text fields and 'Cancel' and 'Next' buttons

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,14 @@
+/*
+ * SurveyField contains logic to render a single label and text input
+ */
+
+import React from "react";
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,12 +2,52 @@
  * SurveyForm shows a form to user to add input
  */
 
+import _ from "lodash";
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recepient List", name: "emails" }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
   render() {
-    return <div>Survey Form!!</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link
+            to="/surveys"
+            className="btn-flat left white-text"
+            style={{ backgroundColor: "#dd4b39" }}
+          >
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
